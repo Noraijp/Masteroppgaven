355dac6a76eb81d4df622c677e1944350a15cd13
Nora Pettersen <n.i.j.pettersen@fys.uio.no>
2020-02-17 17:41:01 +0100

    updated files

--- Jon code/reaction_example.py
+++ Jon code/reaction_example.py
@@ -37,12 +37,12 @@ def calculate_flux(csv_list, reaction_list, target_list, product_list, mass_33Me
 
 	for i in range(len(reaction_list)):
 		activity_data = read_csv(csv_list[i])
-		print('CSV data: \n',activity_data)
+		#print('CSV data: \n',activity_data)
 
 		activity_16MeV = activity_data[:,0]
 		activity_33MeV = activity_data[:,1]
-		print('16 MeV data: \n',activity_16MeV)
-		print('33 MeV data: \n',activity_33MeV)
+		#print('16 MeV data: \n',activity_16MeV)
+		#print('33 MeV data: \n',activity_33MeV)
 
 
 		### starting with yttrium monitor
@@ -57,8 +57,11 @@ def calculate_flux(csv_list, reaction_list, target_list, product_list, mass_33Me
 
 
 
+		dc_33MeV =      DecayChain(product_list[i], R=1, time=530.0)
+		dc_33MeV.append(DecayChain(product_list[i],      time=((5.0*60.0)+40.0)))
+		dc_33MeV.append(DecayChain(product_list[i], R=1, time=6750.0))
 
-		dc_33MeV = npat.DecayChain(product_list[i], R=1, time=irr_time_33MeV)
+#		dc_33MeV = npat.DecayChain(product_list[i], R=1, time=irr_time_33MeV)
 		dc_33MeV.append(npat.DecayChain(product_list[i], time=1E6))
 
 		count_start_time = 0.0 # h
@@ -91,6 +94,8 @@ def calculate_flux(csv_list, reaction_list, target_list, product_list, mass_33Me
 
 
 
+
+
 irr_time_33MeV = 1.5*3600.0 #sec
 
 
@@ -119,9 +124,9 @@ mass_33MeV = 0.5443 #g
 unc_mass_33MeV = 0.0013  #g
 
 ### Search the IRDFF neutron library for reactions
-lb = Library('IRDFF')
+#lb = Library('IRDFF')
 # print(lb.search(target='113IN'))
-print(lb.search(product='116INm'))
+#print(lb.search(product='116INm'))
 # print(lb.search(target='113IN',product='114INm1'))
 
 in_avg_flux, in_unc_avg_flux = calculate_flux(csv_list, reaction_list, target_list, product_list, mass_33MeV, unc_mass_33MeV)
@@ -131,30 +136,108 @@ print('In flux uncertinty : ', in_unc_avg_flux)
 
 
 
+### for Aluminum
 
-#
-# ### for zinc
-#
-#
-# dc = npat.DecayChain('67CU', R=1, time=irr_time)
-# dc.append(npat.DecayChain('67CU', time=1E6))
-#
-# count_start_time = 2.0 # h
-# measured_A0_67ZN_activity = 3.7E3 # bq
-#
-# R = measured_A0_67ZN_activity/dc.activity('67CU', t=count_start_time, units='h')
-#
-#
-# mass = 1.5
-# itp = npat.Isotope('67ZN')
-# ab = 1E-2*itp.abundance()
-# n_atoms = (ab*mass*6.022E-1)/itp.mass
-#
-#
-# xs = R/(n_atoms*avg_flux)
-# print(xs)
-#
-#
-# rx = npat.Reaction('67ZN(n,p)67CU', library='IRDFF')
-# meulders_33MeV_xs_avg = 1E-3*rx.average(meulders_33MeV[:,0], meulders_33MeV[:,1])
-# print(meulders_33MeV_xs_avg)
+csv_list      = ['Al_24Na.csv']
+reaction_list = ['27AL(n,a)24NA']
+target_list   = ['27AL']
+product_list  = ['24NA']
+mass_33MeV = 0.2563 #g
+unc_mass_33MeV = 0.0015  #g
+
+### Search the IRDFF neutron library for reactions
+lb = Library('IRDFF')
+# print(lb.search(target='113IN'))
+print(lb.search(product='24Na'))
+# print(lb.search(target='113IN',product='114INm1'))
+
+al_avg_flux, al_unc_avg_flux = calculate_flux(csv_list, reaction_list, target_list, product_list, mass_33MeV, unc_mass_33MeV)
+print('Al flux            : ', al_avg_flux)
+print('Al flux uncertinty : ', al_unc_avg_flux)
+
+
+
+
+### for Zirconium
+
+csv_list      = ['Zr_89Zr.csv']
+reaction_list = ['90ZR(n,2n)89ZR']
+target_list   = ['90ZR']
+product_list  = ['89ZR']
+mass_33MeV = 0.7557 #g
+unc_mass_33MeV = 0.0012  #g
+
+### Search the IRDFF neutron library for reactions
+#lb = Library('IRDFF')
+# print(lb.search(target='113IN'))
+#print(lb.search(product='89Zr'))
+# print(lb.search(target='113IN',product='114INm1'))
+
+zr_avg_flux, zr_unc_avg_flux = calculate_flux(csv_list, reaction_list, target_list, product_list, mass_33MeV, unc_mass_33MeV)
+print('Zr flux            : ', zr_avg_flux)
+print('Zr flux uncertinty : ', zr_unc_avg_flux)
+
+
+all_fluxes = []
+all_unc_fluxes = []
+
+for i in (y_avg_flux, in_avg_flux, al_avg_flux, zr_avg_flux):
+	all_fluxes.extend(i[:])
+for i in (y_unc_avg_flux, in_unc_avg_flux, al_unc_avg_flux, zr_unc_avg_flux):
+	all_unc_fluxes.extend(i[:])
+print(all_fluxes)
+# print(all_fluxes[3])
+x_index = range(len(all_fluxes))
+
+
+approximate_average_flux = np.average(all_fluxes, weights=1/np.square(all_unc_fluxes))
+unc_approximate_average_flux = np.average(all_unc_fluxes)
+print('approximate_average_flux: ',approximate_average_flux)
+
+# plt.errorbar(x_index, all_fluxes, yerr=all_unc_fluxes, marker='.', linestyle='')
+# plt.plot(x_index, approximate_average_flux*np.ones(len(x_index)), color='red')
+#plt.show()
+
+
+
+
+
+### for zinc
+product_name='67CU'
+
+product_activity_data = read_csv('Zn_67Cu.csv')
+#print('CSV data: \n',activity_data)
+
+product_activity_16MeV = product_activity_data[:,0]
+product_activity_33MeV = product_activity_data[:,1]
+
+
+product_dc_33MeV =      DecayChain(product_name, R=1, time=530.0)
+product_dc_33MeV.append(DecayChain(product_name,      time=((5.0*60.0)+40.0)))
+product_dc_33MeV.append(DecayChain(product_name, R=1, time=6750.0))
+
+#dc = npat.DecayChain(product_name, R=1, time=irr_time)
+product_dc_33MeV.append(npat.DecayChain(product_name, time=1E6))
+
+count_start_time = 0.0 # h
+#measured_A0_67ZN_activity = 3.7E3 # bq
+
+#R = product_activity_33MeV[0]/product_dc_33MeV.activity(product_name, t=count_start_time, units='h')
+R = product_activity_33MeV[0]
+
+mass = 0.8427 #g
+itp = npat.Isotope('67ZN')
+ab = 1E-2*itp.abundance()
+
+n_atoms = (ab*mass*6.022E-1)/itp.mass
+
+
+xs = R/(n_atoms*approximate_average_flux)
+print('Our measured cross section (mb); ', xs)
+
+lb = Library('IRDFF')
+#print(lb.search(target='113IN'))
+print(lb.search(product='67CU'))
+rx = npat.Reaction('67ZN(n,p)67CU', library='IRDFF')
+meulders_33MeV_xs_avg = rx.average(meulders_33MeV[:,0], meulders_33MeV[:,1])
+print('meulders_33MeV_xs_avg: ',meulders_33MeV_xs_avg)
